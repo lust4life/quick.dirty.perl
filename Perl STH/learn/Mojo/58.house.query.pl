49b4cdc8ed4b461c73c0bb5e96b1940791a296ef
$+j <lust4life.jun@gmail.com>
2015-06-24 22:19:17 +0800

    delay request done.

--- Perl STH/learn/Mojo/58.house.query.pl
+++ Perl STH/learn/Mojo/58.house.query.pl
@@ -15,6 +15,33 @@ use Timer::Simple;
 use Try::Tiny;
 
 
+# Mojo::IOLoop::Delay->new->steps(
+
+#                                 # First step (simple timer)
+#                                 sub {
+#                                     my $delay = shift;
+#                                     Mojo::IOLoop->timer(2 => $delay->begin);
+#                                     say 'Second step in 2 seconds.';
+#                                 },
+
+#                                 # Second step (concurrent timers)
+#                                 sub {
+#                                     my ($delay, @args) = @_;
+#                                     Mojo::IOLoop->timer(1 => $delay->begin);
+#                                     Mojo::IOLoop->timer(3 => $delay->begin);
+#                                     say 'Third step in 3 seconds.';
+#                                 },
+
+#                                 # Third step (the end)
+#                                 sub {
+#                                     my ($delay, @args) = @_;
+#                                     say 'And done after 5 seconds total.';
+#                                 }
+#                                )->wait;
+
+# exit;
+
+
 #my $page = path('C:\Users\jiajun\Desktop\test.html');
 #my $mojo_dom = Mojo::DOM->new($page->slurp_utf8);
 
@@ -59,7 +86,7 @@ sub grap_detail_page{
 sub generate_detail_page_url_hash_ref{
     my ($page_list_dom)  = @_;
     #    my $page_list_dom = Mojo::DOM->new(path('c:/users/jiajun/desktop/test1.html')->slurp_utf8);
-    my $detail_url_nums;
+
     my %detail_page_url_hash;
     $page_list_dom->find("div#infolist tr[logr] h1>a[href]:nth-child(1)")
             ->map(attr=>'href')
@@ -69,8 +96,6 @@ sub generate_detail_page_url_hash_ref{
                            return;
                        }
 
-                       ++$detail_url_nums;
-
                        if ($url =~ m</(\d+)x\.shtml>) {
                            my $puid = $1;
                            $detail_page_url_hash{$puid} = $url;
@@ -79,9 +104,10 @@ sub generate_detail_page_url_hash_ref{
                            push @{$detail_page_url_hash{'special-url'}},$url;
                        }
                    });
-    say "has details urls => $detail_url_nums";
+
     return \%detail_page_url_hash;
 }
+
 
 my @page_infos ;
 my %error_query;
@@ -90,17 +116,40 @@ my $url_num ;
 
 my $time_used = Timer::Simple->new();
 
+sub process_detail_result{
+    my ($ua,$result) = @_;
+    my $detail_page_dom = $result->res->dom;
+    my $url = $result->req->url->to_string;
+    try{
+        my $page_info = grap_detail_page($detail_page_dom);
+        $page_info->{'url'} = $url;
+        push @page_infos,$page_info;
+    }catch{
+        $error_query{error_counts} = ++$error_num;
+        my $error_info = $result->res->error;
+        $error_info->{'exception'} = $_;
+        $error_query{$url} = $error_info;
+
+                                #                                say p $result->res; exit;
+                                #                carp "error happened: $url  $_ :";
+    };
+}
+
+
 Mojo::IOLoop->delay(
                     sub{
                         my $delay = shift;
                         for (1..1) {
                             my $page_list_url = sprintf('http://cd.58.com/chuzu/pn%d/',$_);
-                            $ua->get($page_list_url => $delay->begin);
+                            my $end = $delay->begin(0);
+                            $ua->get($page_list_url => sub{
+                                         my ($ua,$tx) = @_;
+                                         $end->($tx->res->dom);
+                                     });
                         }
                     },
                     sub{
-                        my ($delay,@page_list_results) = @_;
-                        my @page_list_doms = map {$_->res->dom} @page_list_results;
+                        my ($delay,@page_list_doms) = @_;
 
                         for my $page_list_dom (@page_list_doms) {
                             my $detail_page_url_hash_ref = generate_detail_page_url_hash_ref($page_list_dom);
@@ -110,45 +159,23 @@ Mojo::IOLoop->delay(
 
                                 if ($puid eq 'special-url') {
                                     for my $detail_page_url (@{$detail_page_url_hash_ref->{'special-url'}}) {
+                                        #                                        my $end = $delay->begin(0);
                                         ++$url_num;
-                                        my $delay_time = $url_num * 0.5;
-                                        Mojo::IOLoop->timer( $delay_time => $delay->pass($detail_page_url));
+                                        my $delay_time = $url_num * 0.3;
+                                        Mojo::IOLoop->timer( $delay_time => sub{
+                                                                 $ua->max_redirects(2)->get($detail_page_url => \&process_detail_result);
+                                                             });
                                     }
                                 } else {
                                     my $detail_page_url = $detail_page_url_hash_ref->{$puid};
+                                    my $end = $delay->begin(0);
                                     ++$url_num;
-                                    my $delay_time = $url_num * 0.5;
-                                    Mojo::IOLoop->timer($delay_time => $delay->pass($detail_page_url));
+                                    my $delay_time = $url_num * 0.3;
+                                    Mojo::IOLoop->timer($delay_time => sub{
+                                                            $ua->max_redirects(2)->get($detail_page_url => \&process_detail_result);
+                                                        });
                                 }
                             }
-                        }
-                    },
-                    sub{
-                        my ($delay,@detail_page_urls) = @_;
-                        say $time_used;
-                        exit;
-                        for my $detail_page_url(@detail_page_urls){
-                            $ua->max_redirects(2)->get($detail_page_url => $delay->begin);
-                        }
-                    },
-                    sub{
-                        my ($delay,@detail_results) = @_;
-                        for my $result (@detail_results) {
-                            my $detail_page_dom = $result->res->dom;
-                            my $url = $result->req->url->to_string;
-                            try{
-                                my $page_info = grap_detail_page($detail_page_dom);
-                                $page_info->{'url'} = $url;
-                                push @page_infos,$page_info;
-                            }catch{
-                                $error_query{error_counts} = ++$error_num;
-                                my $error_info = $result->res->error;
-                                $error_info->{'exception'} = $_;
-                                $error_query{$url} = $error_info;
-
-                                #                                say p $result->res; exit;
-                                #                carp "error happened: $url  $_ :";
-                            };
                         }
                     }
                    )->wait;
@@ -157,5 +184,5 @@ my $page_info_json = encode_json(\@page_infos);
 $cwd->path("/result.json")->spew($page_info_json);
 
 say p %error_query;
-say "\ndone!time used: $time_used";
+say "\ndone!\ntime used: $time_used";
 say "page_info counts => " . scalar(@page_infos) . "\ntotal urls => $url_num";
