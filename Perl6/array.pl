3d00a320016a46d7efccb492b4150d928912ebe4
$+j <lust4life.jun@gmail.com>
2015-09-12 21:39:03 +0800

    array learn

--- Perl6/array.pl
+++ Perl6/array.pl
@@ -6,5 +6,9 @@ say "hello here is : {join('-',@names)} ";
 say @names;
 
 for @names -> $name {
-	say "my name is => $name";
+    say "my name is => $name";
 }
+
+my @lines = "array.pl".IO.lines;
+
+.say for keys(@lines ∩ ("use v6;", 'say @names;'));
